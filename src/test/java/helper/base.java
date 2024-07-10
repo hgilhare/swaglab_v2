16addae5538ac7b8bdd5c98b04c0212f557a9634
Himanshu Gilhare <124993010+hgilhare@users.noreply.github.com>
2024-07-10 23:01:52 +0530

    Revert "google test"

--- src/test/java/helper/base.java
+++ src/test/java/helper/base.java
@@ -1,9 +1,6 @@
 package helper;
 
 import com.beust.jcommander.Parameter;
-import com.google.cloud.functions.HttpFunction;
-import com.google.cloud.functions.HttpRequest;
-import com.google.cloud.functions.HttpResponse;
 import io.cucumber.java.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
@@ -36,13 +33,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
-public class base implements HttpFunction {
+public class base {
     public static WebDriver driver;
     public static Properties prop;
     public static Process pro;
     public static ExecutorService executor;
-
-    static base b= new base();
 
 
 
@@ -69,12 +64,9 @@ public class base implements HttpFunction {
     }
 
     @BeforeAll
-    public static void docker_grid_start(HttpRequest req,HttpResponse resp) throws Exception {
-        b.service(req, resp);
+    public static void docker_grid_start() throws IOException, InterruptedException {
 
-
-
-
+        System.out.println("working");
 
 
 
@@ -97,6 +89,18 @@ public class base implements HttpFunction {
 
 
 
+        String browserName = prop.getProperty("browser");
+        if (browserName.equalsIgnoreCase("chrome")) {
+           ChromeOptions option =new ChromeOptions();
+           option.addArguments("--headless");
+
+            driver = new ChromeDriver(option);
+
+        }else if(browserName.equalsIgnoreCase("firefox")) {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444"), new FirefoxOptions());
+        }else if(browserName.equalsIgnoreCase("edge")) {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444"), new EdgeOptions());
+        }
 
 
 
@@ -108,7 +112,7 @@ public class base implements HttpFunction {
     @AfterAll
     public static void docker_grid_stop() throws IOException, InterruptedException {
 
-
+        System.out.println("finish");
 
 
 //        executor.shutdown();
@@ -209,20 +213,4 @@ public class base implements HttpFunction {
     }
 
 
-    @Override
-    public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
-        String browserName = prop.getProperty("browser");
-        if (browserName.equalsIgnoreCase("chrome")) {
-            ChromeOptions option =new ChromeOptions();
-            option.addArguments("--headless");
-
-            driver = new ChromeDriver(option);
-
-        }else if(browserName.equalsIgnoreCase("firefox")) {
-            driver = new RemoteWebDriver(new URL("http://localhost:4444"), new FirefoxOptions());
-        }else if(browserName.equalsIgnoreCase("edge")) {
-            driver = new RemoteWebDriver(new URL("http://localhost:4444"), new EdgeOptions());
-        }
-
-    }
 }
