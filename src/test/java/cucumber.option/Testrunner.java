3517513841df083069be561f8938cf2f397eec29
gouravgilhare <GOuravgilhare4@gmail.com>
2024-07-17 23:15:12 +0530

    allure

--- src/test/java/cucumber.option/Testrunner.java
+++ src/test/java/cucumber.option/Testrunner.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/feature", glue = { "stepdefination", "helper" },
 
-        tags = "@Reg", plugin = { "pretty", "html:target/htmlreport/test.html" })
+        tags = "@Reg", plugin = { "pretty", "html:target/htmlreport/test.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" })
 public class Testrunner {
 
 }
